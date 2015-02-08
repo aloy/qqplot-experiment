3cb686a3c40c5e3553e3ede39175ce77523cf614
Heike Hofmann <hofmann@iastate.edu>
2015-02-08 10:07:31 -0600

    TS confidence bands

--- code/createPlots.R
+++ code/createPlots.R
@@ -1,3 +1,16 @@
+#-------------------------------------------------------------------------------
+# Script generating lineups of Q-Q plots: standard and detrended versions
+#
+# Heike Hofmann, Adam Loy
+# February 2015
+#
+# References: 
+#   Aldor-Noiman, S., Brown, L.D., Buja, A., Rolke, W., and Stine, R.A. (2013), 
+#   “The Power to See: A New Graphical Test of Normality,” The American Statistician, 
+#   67, 249–260.
+#-------------------------------------------------------------------------------
+
+
 setwd("~/Dropbox/lineups-nf/turk17/")
 library(ggplot2)
 library(lme4)     # for modeling
@@ -8,6 +21,11 @@ library(plyr)
 library(reshape2)
 library(stringr)
 library(gridSVG)
+
+# required to TS test bands
+library(mvtnorm)    # for the multivariate normal distribution
+library(robustbase) # for robust estimates for the mean and sd
+
 
 scriptURL = "http://www.hofroe.net/examples/lineup/qqaction.js"
 
@@ -46,6 +64,21 @@ std_lineup <- function(dframe) {
   )  
 }
 
+std_ts_lineup <- function(dframe) {
+  require(ggplot2)
+  require(grid)
+  print(ggplot(aes(x=ts.qq.x, y=ts.qq.y), data = dframe) + 
+          #  geom_smooth(aes(naive1.qq.x, naive1.env.fit.value), colour="grey50", se=FALSE, method="loess") +
+          geom_abline(colour="grey50") +
+          facet_wrap(~.sample, ncol=5) +
+          geom_point() + 
+          theme_bw() + xlab("") + ylab("") +
+          geom_ribbon(aes(x = ts.qq.x, ymin = ts.qq.lower, ymax = ts.qq.upper),alpha = .2)+
+          theme(axis.text.y = element_blank(), axis.text.x = element_blank(),
+                axis.ticks = element_blank(), plot.margin=unit(c(0,0,0,0), "cm"))
+  )  
+}
+
 rot2_lineup <- function(dframe) {
   require(ggplot2)
   require(grid)
@@ -57,6 +90,21 @@ rot2_lineup <- function(dframe) {
                             ymin = naive1.env.lower-naive1.env.fit.value, ymax = naive1.env.upper-naive1.env.fit.value),alpha = .2)+
             theme_bw() + xlab("") + ylab("") +
             ylim(range(dframe$naive1.qq.y)) +
+            theme(axis.text.y = element_blank(), axis.text.x = element_blank(),
+                  axis.ticks = element_blank(), plot.margin=unit(c(0,0,0,0), "cm")))
+}
+
+rot2_ts_lineup <- function(dframe) {
+  require(ggplot2)
+  require(grid)
+  print(  ggplot(aes(x=ts.qq.x, y=ts.qq.y-ts.qq.x), data = dframe) + 
+            facet_wrap(~.sample, ncol=5) + 
+            geom_hline(yintercept=0, colour="grey30")+
+            geom_point() + 
+            geom_ribbon(aes(x = ts.qq.x, 
+                            ymin = ts.qq.lower-ts.qq.x, ymax = ts.qq.upper-ts.qq.x),alpha = .2)+
+            theme_bw() + xlab("") + ylab("") +
+            ylim(range(dframe$ts.qq.y)) +
             theme(axis.text.y = element_blank(), axis.text.x = element_blank(),
                   axis.ticks = element_blank(), plot.margin=unit(c(0,0,0,0), "cm")))
 }
@@ -77,6 +125,20 @@ rot_lineup <- function(dframe) {
                   axis.ticks = element_blank(), plot.margin=unit(c(0,0,0,0), "cm")))
 }
 
+rot_ts_lineup <- function(dframe) {
+  require(ggplot2)
+  require(grid)
+  print(  ggplot(aes(x=ts.qq.x, y=ts.qq.y-ts.qq.x), data = dframe) + 
+            facet_wrap(~.sample, ncol=5) + 
+            geom_hline(yintercept=0, colour="grey30")+
+            geom_point() + 
+            geom_ribbon(aes(x = ts.qq.x, 
+                            ymin = ts.qq.lower-ts.qq.x, ymax = ts.qq.upper-ts.qq.x),alpha = .2)+
+            theme_bw() + xlab("") + ylab("") +
+            theme(axis.text.y = element_blank(), axis.text.x = element_blank(),
+                  axis.ticks = element_blank(), plot.margin=unit(c(0,0,0,0), "cm")))
+}
+
 ctrl_lineup <- function(dframe) {
   require(ggplot2)
   require(grid)
@@ -90,6 +152,52 @@ ctrl_lineup <- function(dframe) {
   )
 }
 
+###############################################################
+# The confidence band function                                #
+# The function returns a list with the following:             #
+# 1. lower,upper      the normal scale confidence bands       #
+# 2. individual.alpha the final individual significance level #
+###############################################################
+
+QQ.cb<-function(x.sample,mu=0,sigma=1,M.sim=1000,alpha=0.05,plot=TRUE){
+  
+  n<-length(x.sample)
+  upper.ci<-rep(NA,n)
+  lower.ci<-rep(NA,n)
+  p.value <-matrix(NA,nrow=n,ncol=M.sim)
+  sim=NULL
+  
+  # simulate data
+  for(i in 1:M.sim)
+    sim<-cbind(sim,sort(runif(n)))
+  
+  # widen the CI to get a simultanoues 1-alpha CI
+  for(i in 1:n){
+    tmp<-pbeta(sim[i,],shape1=i,shape2=n+1-i)
+    p.value[i,]<-apply(cbind(tmp,1-tmp),1,min)
+  }
+  
+  critical.values<-apply(p.value,2,min)
+  C.crit<-quantile(critical.values,prob=alpha)
+  
+  upper.ci<-qbeta(1-C.crit,shape1=1:n,shape2=n+1-(1:n))
+  lower.ci<-qbeta(C.crit,shape1=1:n,shape2=n+1-(1:n))
+  
+  
+  # now translate back to normal
+  norm.upper<-qnorm(upper.ci)
+  norm.lower<-qnorm(lower.ci)
+  
+  q.prob<-qnorm((1:n)/(n+1))
+  z.sample<-(x.sample-mu)/sigma
+  if(plot==TRUE){
+    plot(q.prob,norm.upper,type="l",col="red",ylim=c(-3,3),xlim=c(-3,3),ylab="Sample Quantile",xlab="Sample Quantile")
+    lines(q.prob,norm.lower,col="red")
+    points(q.prob,sort(z.sample),pch=19,cex=0.6)
+  }
+  
+  return(data.frame(x=q.prob, y=sort(z.sample), lower=norm.lower, upper=norm.upper))
+}
 
 
 sim_env <- function(x, conf = .95, line=FALSE){
@@ -147,16 +255,19 @@ files <- dir("lineups/data")
 for (fname in files) {
   dframe <- read.csv(sprintf("lineups/data/%s", fname))
   
-  for (dsnfun in c("std_lineup", "rot_lineup", "rot2_lineup", "ctrl_lineup")) {
-    fit2 <- dframe
-    b <- (HLMdiag:::qqlineInfo(dframe$x[dframe$.n==20]))[2] # need motivation for that
-    fit2$x[fit2$.n==20] <- dframe$x[dframe$.n==20]/b # change variance to 1, compare then
-    idx <- grep("naive1.qq", names(fit2))
-    fit2 <- ddply(fit2[,-idx], .(.n), transform, naive1.qq=qqnorm(x, plot.it=FALSE))
-    idx <- grep("naive1.env", names(fit2))
-    fit2 <- ddply(fit2[,-idx], .(.n), transform, naive1.env=sim_env(x))
-    
-    res <- parse_filename(gsub(".csv", "", fname))
+  fit2 <- dframe
+  b <- (HLMdiag:::qqlineInfo(dframe$x[dframe$.n==20]))[2] # slope of the real data determines slope of all
+  fit2$x[fit2$.n==20] <- dframe$x[dframe$.n==20]/b # change variance to 1, compare then
+  idx <- grep("naive1.qq", names(fit2))
+  fit2 <- ddply(fit2[,-idx], .(.n), transform, 
+                naive1.qq=qqnorm(x, plot.it=FALSE))
+  idx <- grep("naive1.env", names(fit2))
+  fit2 <- ddply(fit2[,-idx], .(.n), transform, 
+                naive1.env=sim_env(x),
+                ts.qq=QQ.cb(x, plot=FALSE))
+  res <- parse_filename(gsub(".csv", "", fname))
+
+  for (dsnfun in c("std_lineup", "std_ts_lineup", "rot_lineup", "rot_ts_lineup", "rot2_lineup", "rot2_ts_lineup", "ctrl_lineup")) {
     # outer panel  
     fit2$.sample <- fit2$.sample_outer
     eval(as.symbol(dsnfun))(fit2)    
@@ -210,3 +321,4 @@ for (fname in files) {
   
   
 }
+
